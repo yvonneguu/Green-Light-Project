1fda13822b409cb202c749f27aa0d4bd369424e9
Yvonne Guu <yvonneguu05@gmail.com>
2020-08-11 14:49:06 -0700

    Initial Commit

---
+++ Green Light Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Green Light Project
+//
+//  Created by Yvonne Guu on 8/11/20.
+//  Copyright © 2020 Yvonne Guu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Green Light ProjectTests/Green_Light_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Green_Light_ProjectTests.swift
+//  Green Light ProjectTests
+//
+//  Created by Yvonne Guu on 8/11/20.
+//  Copyright © 2020 Yvonne Guu. All rights reserved.
+//
+
+import XCTest
+@testable import Green_Light_Project
+
+class Green_Light_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
