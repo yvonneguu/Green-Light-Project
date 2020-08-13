420cc32ad773b702b4f10deff78d84787c85c845
Grace Chang <grace.chang313@gmail.com>
2020-08-12 22:35:20 -0700

    August 12th 2020 10:35

---
+++ DRiskFactorsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DRiskFactorsViewController.swift
+//  Green Light Project
+//
+//  Created by Grace Chang on 8/12/20.
+//  Copyright © 2020 Yvonne Guu. All rights reserved.
+//
+
+import UIKit
+
+class DRiskFactorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
